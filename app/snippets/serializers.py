475d50a10159af1bc382020414f5225481037f3a
parkhongbeen <pack122@naver.com>
2020-07-21 14:44:29 +0900

    part 6 the tutorial

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -6,15 +6,16 @@ from snippets.models import Snippet
 
 class SnippetSerializer(serializers.Serializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'highlight', 'id', 'title', 'code', 'linenos', 'language', 'style']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
